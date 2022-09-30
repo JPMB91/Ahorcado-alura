a03606fe03df9ef69cdbebad6e069b8edc3fe55a
Paulo <juan.maldonado@live.cl>
2022-09-30 00:07:26 -0300

    código principal funcionando

--- canvas.js
+++ canvas.js
@@ -29,3 +29,115 @@ function dibujarLinea(){
     pantalla.stroke();
     pantalla.closePath();
 }
+
+//funcion para dibujar la horca
+
+function dibujarHorca(){
+    pantalla.lineWidth = 8;
+ 
+    switch (intentos) {
+       case 0:
+        pantalla.strokeStyle = "black";
+          pantalla.beginPath();
+          pantalla.moveTo(50, 450);//base horca
+          pantalla.lineTo(300, 450);
+          intentos++;
+          break;
+ 
+       case 1:
+        pantalla.strokeStyle = "black";
+          pantalla.moveTo(100, 450);
+          pantalla.lineTo(100, 50);
+          intentos++;
+          break;
+ 
+       case 2:
+        pantalla.strokeStyle = "black";
+          pantalla.moveTo(100, 50);
+          pantalla.lineTo(250, 50);
+          intentos++;
+          break;
+ 
+       case 3:
+        pantalla.strokeStyle = "black";
+          pantalla.moveTo(250, 50)
+          pantalla.lineTo(250, 100);
+          intentos++;
+          break;
+ 
+       case 4:
+        pantalla.strokeStyle = "blue";
+          pantalla.moveTo(250, 100)
+          pantalla.arc(250, 125, 25, 0, 2*Math.PI);
+          intentos++;
+          break;
+ 
+    
+       case 5:
+        pantalla.strokeStyle = "blue";
+          pantalla.moveTo(250, 150);
+          pantalla.lineTo(250, 270);
+          intentos++;
+          break;
+ 
+       case 6:
+        pantalla.strokeStyle = "blue";
+          pantalla.moveTo(250, 175);
+          pantalla.lineTo(210, 200);
+          intentos++;
+          break;
+ 
+       case 7:
+        pantalla.strokeStyle = "blue";
+          pantalla.moveTo(250, 175);
+          pantalla.lineTo(290, 200);
+          intentos++;
+          break;
+ 
+       case 8:
+        pantalla.strokeStyle = "blue";
+          pantalla.moveTo(250, 270);
+          pantalla.lineTo(200, 320);
+          intentos++;
+          perdiste();
+          break;
+ 
+       case 9:
+          pantalla.strokeStyle = "blue";
+          pantalla.moveTo(250, 270);
+          pantalla.lineTo(295, 320);
+          intentos++;
+          perdiste()
+          break;
+    }
+    
+    pantalla.closePath();
+    pantalla.stroke();                   
+ }    
+   
+
+//funcion para dibujar al ahorcado. posiblemente deba cambiar la Y de todo en 100 o mas
+
+
+function escribirLetraCorrecta(index){ //pq index dentro del parentesis
+    pantalla.font = "bold 63px Inter";
+    pantalla.lineWidth = 6;
+    pantalla.lineCap = "round";
+    pantalla.lineJoin = "round";
+    pantalla.fillStyle = "green";
+
+    let anchura = 600/palabraSecreta.length
+        pantalla.fillText(palabraSecreta[index],505+(anchura*index),620) //escribe la palabra secreta separandola en letras (index)  x 535 y 620
+        pantalla.stroke()
+        pantalla.closePath()
+}
+
+function escribirLetraIncorrecta(letra, erroresLeft){
+    pantalla.font = "bold 40px Inter";
+    pantalla.lineWidth = 6;
+    pantalla.lineCap= "round";
+    pantalla.lineJoin="round";
+    pantalla.fillStyle="red";
+    pantalla.fillText(letra, 535 +(40*(10-erroresLeft)),710,40)
+     
+}
--- javascript.js
+++ javascript.js
@@ -1,42 +1,147 @@
 //selectores o variables globales
-let palabras=["ALURA", "ORACLE", "HTML", "ONE"];
+let palabras=["ZAPATO", "MONTE", "GLOBO", "RATA", "AURORA", "LAPIZ", "PUERTA"];
 let pantalla = document.getElementById("lienzo").getContext("2d");
 let palabraSecreta="";
+let letras=[];
+let intentos=0;
+let errores=8; // quesesto
+let aciertos=0;
+let LargoSecreto = palabraSecreta.length; //etsperimentando
+let letra=[];
 
 
-//palabra Secreta desde el array "palabras"
 
+//funcion palabra secreta desde el array "palabras"
 function escogerPalabra(){
     let palabra = palabras[Math.floor(Math.random()*palabras.length)];
     palabraSecreta=palabra;
     console.log(palabraSecreta)
 }
 
+function sumarletraIncorrecta(){
+  errores -=1
+}  
+
+//suma aciertos
+function sumarAciertos(){
+  aciertos +=1
+  console.log(aciertos)
+}
+
+    
 //iniciar
-function iniciarJuego(){
-    document.getElementById("inicioJuego").style.display = "none";
-    escogerPalabra()
-    dibujarCanvas()
-    dibujarLinea()
+    function iniciarJuego(){
+        document.getElementById("inicioJuego").style.display = "none";
+        escogerPalabra()
+        dibujarCanvas()
+        dibujarLinea()
+        dibujarHorca()
+        perdiste()
+        
+        document.onkeydown = (e) => {
+            let letra = e.key.toUpperCase()
+              if (verificar(letra) && palabraSecreta.includes(letra)) {
+                  for (let i = 0; i < palabraSecreta.length; i++) {
+                    if (palabraSecreta[i] === letra) {
+                      escribirLetraCorrecta(i);
+                      sumarAciertos(); 
+                      ganaste()       
+                    } 
+                  }                 
+                }
+                else{
+                sumarletraIncorrecta(letra)
+                escribirLetraIncorrecta(letra, errores)
+                dibujarHorca() //aer esta ensala de weas que armé
+                }
+        }
+    
 }
 
-//nueva palabra ingresada por usuario
+function ganaste(){
+  if(aciertos >= palabraSecreta.length){
+      console.log("ganaste")
+      alert("Ganaste");
+     
+  }
+}
 
+
+//probando
+/*document.getElementById("btn-guardarSeguir").onclick = () => {
+ guardarSeguir()
+}*/
+
+//nueva palabra ingresada por usuario se convierte automaticamente a mayusculas
 function nuevaPalabraIngresada(){
-    let palabraIngresada = document.getElementById("ingresarPalabra").value;
-            palabraSecreta = palabraIngresada;
-            console.log(palabraSecreta)
+  let palabraIngresada = document.getElementById("ingresarPalabra").value.toUpperCase();
+      palabraSecreta = palabraIngresada;
+      console.log(palabraSecreta)
 }
 
-//hacer que el boton guardar y continuar inicie el juego 
+
+//hacer que el boton guardar y continuar inicie el juego luego de ingresar palabra
 
 function guardarSeguir(){
-    document.getElementById("ingresarTxt").style.display = "none";
-    nuevaPalabraIngresada()
-    dibujarCanvas()
-    dibujarLinea()
+      //document.getElementById("ingresarPalabra").style.display = "none"; // necesito cambiar los botones que quedan y los que se esconden
+      nuevaPalabraIngresada()
+      dibujarCanvas()
+      dibujarLinea()
+      dibujarHorca()
+      //perdiste()
+      
+      document.onkeydown = (e) => {
+        let letra = e.key.toUpperCase()
+          if (verificar(letra) && palabraSecreta.includes(letra)) {
+              for (let i = 0; i < palabraSecreta.length; i++) {
+                if (palabraSecreta[i] === letra) {
+                  escribirLetraCorrecta(i);
+                  sumarAciertos(); 
+                  ganaste()       
+                }
+                
+              }
+              
+            }
+            else{
+            sumarletraIncorrecta(letra)
+            escribirLetraIncorrecta(letra, errores)
+            dibujarHorca() //aer esta ensala de weas que armé
+            }
+    }
+
+  }
+
+
+
+
+function verificar(key){
+  //let estado= false;
+    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)){
+        letras.push(key);
+        console.log(key);
+        return estado
+        }
+        else{
+            estado = true;
+            console.log(key)
+            return estado;
+         }
 }
 
 
+
+
+function perdiste() {
+    if (intentos == 10) {
+        console.log("PERDISTE");
+        document.removeEventListener("keydown", verificar);
+        alert("Perdiste, la palabra oculta era " + palabraSecreta);
+        //necesito crear algo que lo envie a la pantalla home
+    }
+ }
+
+
+//necesito boton volver a jugar
 
 
